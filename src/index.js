5063d74f92fbedab1743ee10c873c7efb49bd312
Jim Daniels Wasswa <jim@deriv.com>
2022-07-13 16:14:37 +0800

    chore: add check for valid ObjectId

--- src/index.js
+++ src/index.js
@@ -43,6 +43,9 @@ app.get("/todos/", async (req, res) => {
 app.get("/todos/:id", async (req, res) => {
   try {
     const { id } = req.params || 0;
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+      return res.status(404).json();
+    }
     const todo = await Todo.findById(id);
     if (todo) {
       return res.status(200).json({ todo });
